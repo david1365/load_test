38b7de2f8170d8fb5e65e4eb0cc44e1be9ccc37b
d.akbari <daak1365@yahoo.com>
2019-01-22 09:01:32 +0330

    تست بار با توکن

--- grinder/main.py
+++ grinder/main.py
@@ -13,7 +13,11 @@ import calendar
 import uuid
 import random
 
-host = "http://10.12.47.60:8181"
+
+host = "http://10.12.47.73:8282"
+
+loginUrl = '/service/platform/authentication/login'
+refreshTokenUrl = '/service/platform/authentication/refresh'
 
 customerRelationProfileUrl = '/service/de/product/customer-relation-profile'
 productUrl = '/service/de/product/product'
@@ -35,98 +39,79 @@ depositInvoiceCode = 6814812
 #---------------------------------------------------------------------
 depositCustomerRelationDepositDtoId = 1790335
 
+testNumber = 0
+
+headers = [
+    NVPair('Content-Type', 'application/json'),
+    NVPair('Charset', 'UTF-8'),
+]
+
+refreshTokenInterval = 1 #minuts
+
+
+def log(message):
+    grinder.logger.info(message)
+
+def pickUpToken(response):
+    return "JWT " + json.loads(response.getText())["payload"]["content"]["token"]
+
+def calculateRefreshTokenTime(now):
+    return now + timedelta(minutes=refreshTokenInterval)
+
+def createRequest(description):
+    global testNumber
+    global headers
+
+    testNumber += 1
+    request = HTTPRequest(url=host)
+    request.headers = headers
+    Test(testNumber, description).record(request)
+    return request
+
+
+#----get------------------------------------------------------------
+depositRequest_get = createRequest("deposit request(GET)")
+depositInvoiceRequest_get = createRequest("deposit Invoice request(GET)")
+depositCustomerRelationRequest_get = createRequest("deposit Customer Relation request(GET)")
+cifRealCustomerRequest_get = createRequest("cif Real Customer request(GET)")
+platformRequest_get = createRequest("platform request(GET)")
+customerRelationProfileRequest_get = createRequest("customer Relation Profile request(GET)")
+productRequest_get = createRequest("product request(GET)")
+
+#----TokenRequest------------------------------------------------------------
+loginRequest_post = createRequest("login request(POST)")
+refreshTokenRequest_get = createRequest("refreshToken request(GET)")
+
+#----post------------------------------------------------------------
+depositTransferPaymenttRequest_post = createRequest("deposit Transfer Payment request(POST)")
+cifRealCustomerRequest_post = createRequest("cif Real Customer request(POST)")
+platformRequest_post = createRequest("platform request(POST)")
+depositCustomerRelationRequest_post = createRequest("deposit Customer Relation request(POST)")
+depositRequest_post = createRequest("deposit request(POST)")
+customerRelationProfileRequest_post = createRequest("customer Relation Profile request(POST)")
+productRequest_post = createRequest("product request(POST)")
 
 
 
-class UserBehavior :
-    headers = [
-        NVPair('Content-Type', 'application/json'),
-        NVPair('Charset', 'UTF-8'),
-    ]
 
-    testNumber = 0
+class TestRunner:
+    token = None
 
-    def createRequest(self, description):
-        self.testNumber += 1
+    headers = None
 
-        request = HTTPRequest(url=host)
-        request.headers = self.headers
-        Test(self.testNumber, description).record(request, HTTPRequest.getHttpMethodFilter())
-        return request
+    totalTestCount = 15
+
+    testCounter = 1
+
+    refreshTime = None
 
 
-    #----get------------------------------------------------------------
-    def createGetRequest(self):
-        self.depositRequest_get = self.createRequest("deposit request(GET)")
-        self.depositInvoiceRequest_get = self.createRequest("deposit Invoice request(GET)")
-        self.depositCustomerRelationRequest_get = self.createRequest("deposit Customer Relation request(GET)")
-        self.cifRealCustomerRequest_get = self.createRequest("cif Real Customer request(GET)")
-        self.platformRequest_get = self.createRequest("platform request(GET)")
-        self.customerRelationProfileRequest_get = self.createRequest("customer Relation Profile request(GET)")
-        self.productRequest_get = self.createRequest("product request(GET)")
+    def refreshTokenDecider(self):
+        now = datetime.now()
+        if now >= self.refreshTime:
+            self.refreshToken_get(now)
 
-    #--------------------
-    def getRequest(self):
-        self.depositInvoice_get()
-        self.cifRealCustomer_get()
-        self.deposit_customer_relation_get()
-        self.platform_get()
-        self.deposit_get()
-        self.customer_relation_profile_get()
-        self.product_get()
-
-    #----post------------------------------------------------------------
-    def createPostRequest(self):
-        self.depositTransferPaymenttRequest_post = self.createRequest("deposit Transfer Payment request(POST)")
-        self.cifRealCustomerRequest_post = self.createRequest("cif Real Customer request(POST)")
-        self.platformRequest_post = self.createRequest("platform request(POST)")
-        self.depositCustomerRelationRequest_post = self.createRequest("deposit Customer Relation request(POST)")
-        self.depositRequest_post = self.createRequest("deposit request(POST)")
-        self.customerRelationProfileRequest_post = self.createRequest("customer Relation Profile request(POST)")
-        self.productRequest_post = self.createRequest("product request(POST)")
-
-    #--------------------
-    def postRequest(self):
-        self.depositTransferPayment_post()
-        self.cifRealCustomer_post()
-        self.platform_post()
-        self.deposit_customer_relation_post()
-        self.deposit_post()
-        self.customer_relation_profile_post()
-        self.product_post()
-
-
-
-    def call(self):
-        self.createGetRequest()
-        self.createPostRequest()
-
-        self.getRequest()
-        self.postRequest()
-
-
-
-    # def on_start(self):
-    #     """ on_start is called when a Locust start before any task is scheduled """
-    #     self.login()
-    #
-    # def on_stop(self):
-    #     """ on_stop is called when the TaskSet is stopping """
-    #     self.logout()
-    #
-    # def login(self):
-    #     pass
-    #
-    # def logout(self):
-    #     pass
-#------------------------------------------------------------------
-    # def post(self, url, payload):
-    #     return self.request.POST(url, JSONObject(payload), self.headers)#(url, data=json.dumps(payload), headers=self.headers)
-    #
-    # def get(self, url):
-    #     # return self.client.get(url, headers=self.headers)
-    #     return self.request.GET(url)
-#-----------------------------------------------------------------
+    #-----------------------------------------------------------------
     def fakeDate(self):
         dt = datetime.now() + timedelta(days=1)
         unixTimestamp = calendar.timegm(dt.utctimetuple())
@@ -138,29 +123,39 @@ class UserBehavior :
 
     def addCode(self, url, code):
         return url + '/' + str(code)
-#---get------------------------------------------------------------------------------------------
+    #---get------------------------------------------------------------------------------------------
 
     def depositInvoice_get(self):
-        self.depositInvoiceRequest_get.GET(self.addCode(depositInvoiceUrl, depositInvoiceCode))
+        depositInvoiceRequest_get.GET(self.addCode(depositInvoiceUrl, depositInvoiceCode),None, self.headers)
 
     def cifRealCustomer_get(self):
-        self.cifRealCustomerRequest_get.GET(self.addCode(cifRealCustomerUrl, cifRealCustomerCode))
+        cifRealCustomerRequest_get.GET(self.addCode(cifRealCustomerUrl, cifRealCustomerCode), None, self.headers)
 
     def deposit_customer_relation_get(self):
-        self.depositCustomerRelationRequest_get.GET(self.addCode(depositCustomerRelationUrl, depositCustomerRelationCode))
+        depositCustomerRelationRequest_get.GET(self.addCode(depositCustomerRelationUrl, depositCustomerRelationCode), None, self.headers)
 
     def platform_get(self):
-        self.platformRequest_get.GET(self.addCode(platformUrl, platformCode))
+        platformRequest_get.GET(self.addCode(platformUrl, platformCode), None, self.headers)
 
     def deposit_get(self):
-        self.depositRequest_get.GET(self.addCode(getDepositUrl, depositCode))
+        depositRequest_get.GET(self.addCode(getDepositUrl, depositCode), None, self.headers)
 
     def customer_relation_profile_get(self):
-        self.customerRelationProfileRequest_get.GET(self.addCode(customerRelationProfileUrl, customerRelationProfileCode))
+        customerRelationProfileRequest_get.GET(self.addCode(customerRelationProfileUrl, customerRelationProfileCode), None, self.headers)
 
     def product_get(self):
-        self.productRequest_get.GET(self.addCode(productUrl, productCode))
-#----post---------------------------------------------------------------------------------------------
+        productRequest_get.GET(self.addCode(productUrl, productCode), None, self.headers)
+
+    #----Token---------------------------------------------------------------------------------------------
+    def refreshToken_get(self, now):
+        self.refreshTime = calculateRefreshTokenTime(now)
+        response = refreshTokenRequest_get.GET(refreshTokenUrl, None, self.headers)
+        self.token = pickUpToken(response)
+
+    def login_post(self, payload):
+        return loginRequest_post.POST(loginUrl, json.dumps(payload))
+
+    #----post---------------------------------------------------------------------------------------------
     def depositTransferPayment_post(self):
         payload = {
             "sourceDepositCode": random.randint(10, 1000010),
@@ -168,7 +163,7 @@ class UserBehavior :
             "amount": "1"
         }
 
-        self.depositTransferPaymenttRequest_post.POST(depositTransferPaymentUrl, json.dumps(payload))
+        depositTransferPaymenttRequest_post.POST(depositTransferPaymentUrl, json.dumps(payload), self.headers)
 
     def cifRealCustomer_post(self):
         payload = {
@@ -179,15 +174,15 @@ class UserBehavior :
             "nationalCode": 2300588951,
             "shahabCode": None,
             "shahabCodeConfirmed": None,
-            "name": "شش",
-            "lastName": "سس",
-            "unifiedName": "شش*سس",
+            "name": "hasan",
+            "lastName": "taghavi",
+            "unifiedName": "unali",
             "latinName": "Saman",
-            "latinLastName": "Alishiri",
-            "latinUnifiedName": "Saman*Alishiri",
+            "latinLastName": "Alaki",
+            "latinUnifiedName": "Saman*Alaki",
             "nationalityId": 3883,
             "creditCode": None,
-            "stockCode": None,
+            # "stockCode": None,
             "economicCode": None,
             "branchCode": 11,
             "enabled": 1,
@@ -198,7 +193,7 @@ class UserBehavior :
             "registerDate": 583273800000,
             "postalCode": None,
             "postalAddress": None,
-            "fatherName": "حسن",
+            "fatherName": "TAGHI",
             "sex": "1",
             "idNumber": None,
             "seri": None,
@@ -207,7 +202,7 @@ class UserBehavior :
             "idIssueArea": None,
             "idIssueDate": None,
             "idDescription": None,
-            "idIssuePlace": None,
+            "idIssuePlace": 1,
             "birthPlaceId": None,
             "eCard": None,
             "maritalStatus": None,
@@ -231,7 +226,7 @@ class UserBehavior :
             "personnelCode": None
         }
 
-        self.cifRealCustomerRequest_post.POST(cifRealCustomerUrl,  json.dumps(payload))
+        cifRealCustomerRequest_post.POST(cifRealCustomerUrl,  json.dumps(payload), self.headers)
 
     def platform_post(self):
         payload = {
@@ -249,7 +244,7 @@ class UserBehavior :
             "maxOpenedDeposit": 1000
         }
 
-        self.platformRequest_post.POST(platformUrl, json.dumps(payload))
+        platformRequest_post.POST(platformUrl, json.dumps(payload), self.headers)
 
     def deposit_customer_relation_post(self):
         payload = {
@@ -295,7 +290,8 @@ class UserBehavior :
                 "version": None
             }
         }
-        self.depositCustomerRelationRequest_post.POST(depositCustomerRelationUrl, json.dumps(payload))
+
+        depositCustomerRelationRequest_post.POST(depositCustomerRelationUrl, json.dumps(payload), self.headers)
 
     def deposit_post(self):
         payload = {
@@ -331,7 +327,8 @@ class UserBehavior :
             "openerLetter": None,
             "version": None
         }
-        self.depositRequest_post.POST(PostDepositUrl, json.dumps(payload))
+
+        depositRequest_post.POST(PostDepositUrl, json.dumps(payload), self.headers)
 
     def customer_relation_profile_post(self):
         payload = {
@@ -351,7 +348,7 @@ class UserBehavior :
             "legalPersonOwnerTypeProfileId": None
         }
 
-        self.customerRelationProfileRequest_post.POST(customerRelationProfileUrl, json.dumps(payload))
+        customerRelationProfileRequest_post.POST(customerRelationProfileUrl, json.dumps(payload), self.headers)
 
     def product_post(self):
         payload = {
@@ -416,12 +413,70 @@ class UserBehavior :
             "rollOverProfileId": None
         }
 
-        self.productRequest_post.POST(productUrl, json.dumps(payload))
+        productRequest_post.POST(productUrl, json.dumps(payload), self.headers)
+
+    def __init__(self):
+        payload = {
+            "username": "test",
+            "password": "1234"
+        }
+
+        self.refreshTime = calculateRefreshTokenTime(datetime.now())
+
+        response = self.login_post(payload)
+        self.token = pickUpToken(response)
+        # print self.token
+        # log(self.token)
+        # log("initialising")
 
 
-class TestRunner:
+
     def __call__(self):
-        userBehavior = UserBehavior()
-        userBehavior.call()
+        self.headers = [
+            NVPair('Content-Type', 'application/json'),
+            NVPair('Charset', 'UTF-8'),
+            NVPair('Authorization', self.token)
+        ]
+
+        if self.testCounter == 1:
+            self.depositInvoice_get()
+        elif self.testCounter == 2:
+            self.cifRealCustomer_get()
+        elif self.testCounter == 3:
+            self.deposit_customer_relation_get()
+        elif self.testCounter == 4:
+            self.platform_get()
+        elif self.testCounter == 5:
+            self.deposit_get()
+        elif self.testCounter == 6:
+            self.customer_relation_profile_get()
+        elif self.testCounter == 7:
+            self.product_get()
+        elif self.testCounter == 8:
+            self.depositTransferPayment_post()
+        elif self.testCounter == 9:
+            self.cifRealCustomer_post()
+        elif self.testCounter == 10:
+            self.platform_post()
+        elif self.testCounter == 11:
+            self.deposit_customer_relation_post()
+        elif self.testCounter == 12:
+            self.deposit_post()
+        elif self.testCounter == 13:
+            self.customer_relation_profile_post()
+        elif self.testCounter == 14:
+            self.product_post()
+        elif self.testCounter == 15:
+            self.refreshTokenDecider()
+
+        if self.testCounter >= self.totalTestCount:
+            self.testCounter = 1
+        else:
+            self.testCounter += 1
+
+
+            # log(self.token)
+            #log("in __call__()")
+
 
 
